a5e481f69bcb7859efc5466987631c4d475f63b3
uzairansari11 <uzairans532@gmail.com>
2023-05-22 11:19:32 +0530

    updated signin signup and payment page

--- payment/payment.js
+++ payment/payment.js
@@ -1,21 +1,27 @@
 let allCart = JSON.parse(localStorage.getItem("cart")) || [];
-
 let payment = document.querySelector("form");
 let Bag = JSON.parse(localStorage.getItem("pay")) || [];
+
 payment.addEventListener("submit", function (event) {
   event.preventDefault();
-  let obj = {
-    email: payment.Email.value,
-    mobile: payment.mobile.value,
-    pass: payment.pass.value,
-  };
-  Bag.push(obj);
-  if (obj.email == "" || obj.mobile == "" || obj.pass == "") {
-    alert(" please  fill correct card  details");
-  } else {
-    localStorage.setItem("pay", JSON.stringify(Bag));
-    alert("Payment Successful");
 
+  let email = payment.elements["cardNumber"].value;
+  let mobile = payment.elements["cardHolderName"].value;
+  let pass = payment.elements["otp"].value;
+
+  if (email === "" || mobile === "" || pass === "") {
+    alert("Please fill in all the card details correctly.");
+  } else {
+    let obj = {
+      email: email,
+      mobile: mobile,
+      pass: pass,
+    };
+
+    Bag.push(obj);
+    localStorage.setItem("pay", JSON.stringify(Bag));
+
+    alert("Payment Successful");
     window.location.href = "/index.html";
   }
 });
